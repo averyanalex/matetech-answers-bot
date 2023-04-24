752eec30569b0dd3a1a1d9b748bfe17da950c939
AveryanAlex <alex@averyan.ru>
2023-04-24 21:26:02 +0300

    Update engine

--- src/engine.rs
+++
@@ -1,7 +0,0 @@
-pub async fn get_token(login: String, password: String) -> String {
-    "sus".to_owned()
-}
-
-pub async fn get_answers(token: String, test_id: String) -> String {
-    "bruh".to_owned()
-}
