d9c9810e57d8e23e153087389d66e276f5ed8622
AveryanAlex <alex@averyan.ru>
2023-12-06 22:23:43 +0300

    Update engine

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     import-cargo.url = "github:edolstra/import-cargo";
     flake-utils.url = "github:numtide/flake-utils";
     matetech-engine = {
-      url = "git+ssh://git@github.com/cpmbot/engine.git?rev=cb4a7e61170e9c78fab31060a27acdea5097b109";
+      url = "git+ssh://git@github.com/cpmbot/engine.git";
       flake = false;
     };
   };
